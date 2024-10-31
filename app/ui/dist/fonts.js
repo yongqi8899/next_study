fd4c7bbe7a6ba6cb5cca738932917ed1e1bda288
yongqi8899 <1243994415@qq.com>
2024-10-31 18:19:45 +0100

    delete dist

--- app/ui/dist/fonts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.lusitana = exports.inter = void 0;
-var google_1 = require("next/font/google");
-exports.inter = google_1.Inter({ subsets: ['latin'] });
-exports.lusitana = google_1.Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin']
-});
